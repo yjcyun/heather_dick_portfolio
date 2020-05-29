00fdc712182bcaf7b71ec44330da97ba9178ba24
Yujeong Christina Yun <yjc.yun@gmail.com>
2020-05-29 18:57:35 -0400

    enter button on homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'gatsby';
 import styled from 'styled-components';
 import StyledHero from '../components/Home/StyledHero';
 import quoteIcon from '../images/quote.png';
@@ -7,14 +8,15 @@ import quoteIcon from '../images/quote.png';
 export default function Home() {
   return (
     <HomeWrapper>
+      <Link to="/landing" className="btn enter-btn">enter</Link>
       <StyledHero>
-        <button>enter</button>
+        
       </StyledHero>
       <div className="quote-container">
         <div>
           <img src={quoteIcon} alt="quote icon" className="quote-icon" />
           <p className="quote-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea commodi sed quaerat minima nostrum perspiciatis, delectus sequi cumque perferendis earum!</p>
-          <span className="quote-author">- Heather Dick</span>
+          <p className="quote-author">- Heather Dick</p>
         </div>
       </div>
     </HomeWrapper>
@@ -22,28 +24,69 @@ export default function Home() {
 }
 
 const HomeWrapper = styled.div`
+position: relative;
+.enter-btn{
+  text-transform:uppercase;
+  position: absolute;
+  z-index:2;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  background: var(--mainPurple);
+  color: var(--mainWhite);
+  font-size: 1.5rem;
+  transition: var(--transition);
+}
+
+.enter-btn:hover{
+  background: var(--lightPurple);
+}
+
 .quote-container{
-  width: 80vw;
+  width: 85vw;
   max-width: 40rem;
   height: 50vh;
   display: flex;
   justify-content:center;
   align-items: center;
-  margin: -1rem auto;
+  margin: 0 auto;
 }
+
 .quote-icon{
-  width: 3rem;
+  width: 2.3rem;
 }
+
 .quote-text{
-  margin-left: 3rem;
-  font-size: 1.5rem;
+  margin-left: 2rem;
+  font-size: 1.2rem;
 }
+
 .quote-author {
   display:flex;
   justify-content:flex-end;
   font-family: 'Great Vibes', cursive;
   margin-top: 2rem;
-  font-size: 1.3rem;
+  font-size: 1.2rem;
+  letter-spacing: 0.1rem;
 }
 
+@media (min-width: 768px) {
+  .quote-icon{
+    width: 3rem;
+  }
+  .quote-text{
+    margin-left: 3rem;
+    font-size: 1.5rem;
+  }
+}
+
+@media(max-width: 350px) {
+  .quote-icon{
+    width: 1rem;
+  }
+  .quote-text{
+    margin-left: 1rem;
+    font-size: 1rem;
+  }
+}
 `;
