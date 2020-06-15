a69d5f7aacb8c216d02aeb74c8ee5dc2741f6b6b
Yujeong Christina Yun <yjc.yun@gmail.com>
2020-06-15 14:48:04 -0400

    Updated APIurl in config and added package-lock.json on to gitignore

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,7 +36,7 @@ module.exports = {
     {
       resolve: `gatsby-source-strapi`,
       options: {
-        apiURL: `http://localhost:1337`,
+        apiURL: `https://heatherdick.herokuapp.com`,
         queryLimit: 1000, // Default to 100
         contentTypes: [`resumes`, `blogs`, `productions`, `notifications`, `coachings`],
         // singleTypes: [`home-page`, `contact`],
