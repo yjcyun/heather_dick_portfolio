752fef50b967ecabecf6577d757836a8c1ea1688
Yujeong Christina Yun <yjc.yun@gmail.com>
2020-06-16 11:24:42 -0400

    Footer subheading underlined

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -48,22 +48,18 @@ a{
 }
 
 p{
-  margin-bottom: 8px;
   margin-bottom:0.5rem;
+  text-decoration:underline;
 }
 
 .footer-logo {
-  margin-bottom: 8px;
   margin-bottom: 0.5rem;
 }
 
 .footer-content {
-display:-ms-grid;
 display:grid;
--ms-grid-columns: 1fr 1fr;
-    grid-template-columns: 1fr 1fr;
+grid-template-columns: 1fr 1fr;
 grid-row-gap: 1rem;
-margin-top: 16px;
 margin-top: 1rem;
 text-transform: capitalize;
 text-align: left;
@@ -79,55 +75,34 @@ text-align: left;
 }
 
 .footer-contact div{
-  display:-webkit-box;
-  display:-ms-flexbox;
   display:flex;
-  -webkit-box-orient: vertical;
-  -webkit-box-direction: normal;
-      -ms-flex-direction: column;
-          flex-direction: column;
+  flex-direction: column;
 }
 
 .footer-contact small {
-  font-size: 12.8px;
   font-size: 0.8rem;
-  margin-right: 32px;
   margin-right: 2rem;
 }
 
 .nav-links{
-  font-size: 12.8px;
   font-size: 0.8rem;
 }
 
 @media (min-width: 576px) {
   .footer-center{
-    display:-webkit-box;
-    display:-ms-flexbox;
     display:flex;
-    -webkit-box-orient: horizontal;
-    -webkit-box-direction: normal;
-        -ms-flex-direction: row;
-            flex-direction: row;
+    flex-direction: row;
   }
 
   .footer-logo {
-    -webkit-box-flex:2;
-        -ms-flex:2;
-            flex:2;
+    flex:2;
   }
 
   .footer-content {
-    -webkit-box-flex:3;
-        -ms-flex:3;
-            flex:3;
+    flex:3;
     margin-top:0;
-    -webkit-box-orient: horizontal;
-    -webkit-box-direction: normal;
-        -ms-flex-direction: row;
-            flex-direction: row;
-    -ms-grid-columns: 1fr 1fr 1fr;
-        grid-template-columns: 1fr 1fr 1fr;
+    flex-direction: row;
+    grid-template-columns: 1fr 1fr 1fr;
   }
 
   .footer-contact{
