5093aec378fa666db7c2c6aeabbd8c329edb86f8
Yujeong Christina Yun <yjc.yun@gmail.com>
2020-06-21 19:15:52 -0400

    comment out notification for testing

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <div>
       <SEO description="The Home Page for Heather Dick, a Canadian actress, writer, director, Designated Linklater Voice Teacher, voice actor & acting and voice coach. The Home Page for the Sirius Theatrical Company, an independent theatre company in Toronto, Canada"/>
-      <Notification />
+      {/* <Notification /> */}
       <StyledHero />
     </div>
   )
