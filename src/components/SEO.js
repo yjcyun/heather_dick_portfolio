161fcd53f205189cea08ef1a72614c9c52866c78
Christina Yujeong Yun <yjc.yun@gmail.com>
2020-08-09 11:15:30 -0400

    SEO image testing-2

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -37,9 +37,9 @@ const SEO = ({ title, description, image }) => {
       {/* twitter cards */}
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:creator" content={twitterHandle} />
-      <meta name="twitter:title" content={siteTitle} />
-      <meta name="twitter:description" content={siteDescription} />
-      <meta name="twitter:image" content={`${siteUrl}${image}`} />
+      <meta name="twitter:title" content={title || siteTitle} />
+      <meta name="twitter:description" content={description || siteDescription} />
+      <meta name="twitter:image" content={image ||`${siteUrl}${image}`} />
       {/* facebook cards */}
       <meta property="og:url" content={siteUrl} />
       <meta property="og:type" content="website" />
