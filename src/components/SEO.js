208107b67cd1e9ee02617acd8538b5c77aef8f00
Christina Yujeong Yun <yjc.yun@gmail.com>
2020-08-09 12:05:24 -0400

    SEO facebook testing-1

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -39,15 +39,15 @@ const SEO = ({ title, description, image }) => {
       <meta name="twitter:creator" content={twitterHandle} />
       <meta name="twitter:title" content={title || siteTitle} />
       <meta name="twitter:description" content={description || siteDescription} />
-      <meta name="twitter:image:src" content={`${siteUrl}${image}` ||`${siteUrl}${defaultImage}`} />
+      <meta name="twitter:image" content={`${siteUrl}${image}` ||`${siteUrl}${defaultImage}`} />
       {/* facebook card */}
       <meta property="og:url" content={siteUrl} />
       <meta property="og:type" content="website" />
-      <meta property="og:title" content={siteTitle} />
-      <meta property="og:description" content={siteDescription} />
-      <meta property="og:image" content={`${siteUrl}${image}`} />
-      <meta property="og:image:width" content="400" />
-      <meta property="og:image:height" content="300" />
+      <meta property="og:title" content={title || siteTitle} />
+      <meta property="og:description" content={description || siteDescription} />
+      <meta property="og:image" content={`${siteUrl}${image}` ||`${siteUrl}${defaultImage}`} />
+      {/* <meta property="og:image:width" content="400" />
+      <meta property="og:image:height" content="300" /> */}
     </Helmet>
   )
 }
